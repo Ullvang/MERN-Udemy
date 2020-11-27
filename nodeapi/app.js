0945f106c681296399acbe17be9994b38d9d85ea
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-11-27 22:49:17 +0100

    connected to mongodb

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
 // bring in routes
 const postRoutes = require("./routes/post");
@@ -9,7 +23,7 @@ app.use(morgan("dev"));
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
