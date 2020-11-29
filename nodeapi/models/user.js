4fc1cd5527de9457dc86febae939b1a9d3a8159d
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-11-29 22:16:22 +0100

    encrypted password and added virtual fields

--- nodeapi/models/user.js
+++ nodeapi/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { v1: uuidv1 } = require("uuid");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,5 +24,30 @@ const userSchema = new mongoose.Schema({
   },
   updated: Date,
 });
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password, salt);
+  })
+  .get(() => {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: (password) => {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
